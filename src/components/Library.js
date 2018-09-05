7d18aacce6128a2523f277598023677c84713598
Drew Michael Disterhfoft <DrewDisterhoft@Drews-iMac.local>
2018-09-05 17:25:16 -0500

    update.222

--- src/components/Library.js
+++ src/components/Library.js
@@ -5,18 +5,15 @@ import albumData from './../data/albums';
 class Library extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      albums: albumData
-     };
     this.state = { albums: albumData };
   }
   render() {
     return (
-    <section className="library">
+    <section className='library'>
      {
       this.state.albums.map( (album, index) =>
        <Link to={`/album/${album.slug}`} key={index}>
-          img src={album.albumCover} alt={album.title} />
+          <img src={album.albumCover} alt={album.title} />
           <div>{album.title}</div>
           <div>{album.artist}</div>
           <div>{album.songs.length} songs</div>
