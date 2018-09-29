fef9d61d6df270d978ca1447906ffdc6d6c5a68c
Drew Michael Disterhfoft <DrewDisterhoft@Drews-iMac.local>
2018-09-28 20:41:06 -0500

    added volume bar, updated playerbar.js and album.js

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,49 +1,51 @@
 import React, { Component } from 'react';
 
+
 class PlayerBar extends Component {
-
-  formatTime(time) {
-    const timeInSeconds = Math.floor(parseFloat(time))
-    const minutes = Math.floor(timeInSeconds / 60)
-    const remainingSeconds = timeInSeconds - (minutes * 60)
-    return minutes + ":" + remainingSeconds
-  }
-
-  render() {
-    return (
-      <section className="player-bar">
-      <section id="buttons">
-      <button id="previous" onClick={this.props.handlePrevClick}>
-        <span className="ion-skip-backward"></span>
-         </button>
-         <button id="play-pause" onClick={this.props.handleSongClick} >
-         <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
-         </button>
-         <button id="next" onClick={this.props.handleNextClick}>
-           <span className="ion-skip-forward"></span>
-         </button>
-       </section>
-       <section id="time-control">
-       <div className="current-time">{this.formatTime(this.props.currentTime)}</div>
-         <input
-           type="range"
-           className="seek-bar"
-           value={(this.props.currentTime / this.props.duration) || 0}
-           max="1"
-           min="0"
-           step="0.01"
-           onChange={this.props.handleTimeChange}
-         />
-         <div className="total-time">{this.formatTime(this.props.duration)}</div>
-       </section>
-       <section id="volume-control">
-         <div className="icon ion-volume-low"></div>
-         <input type="range" className="seek-bar" value="80" />
-         <div className="icon ion-volume-high"></div>
-       </section>
-      </section>
-    );
-  }
+    render() {
+        return(
+            <section className="player-bar">
+                <div className="player-bar-controls">
+                    <section id="buttons">
+                        <button id="previous" onClick={this.props.handlePrevClick}>
+                            <span className="player-icon ion-ios-skipbackward"></span>
+                        </button>
+                        <button id="play-pause" onClick={this.props.handleSongClick}>
+                            <span className={'play-icon ' + (this.props.isPlaying ? 'ion-ios-pause' : 'ion-ios-play')}></span>
+                        </button>
+                        <button id="next" onClick={this.props.handleNextClick}>
+                            <span className="player-icon ion-ios-skipforward"></span>
+                        </button>
+                    </section>
+                    <section id="time-control">
+                        <div className="current-time">{this.props.formattedTime}</div>
+                        <input
+                            type="range"
+                            className="seek-bar"
+                            value={(this.props.currentTime / this.props.duration) || 0}
+                            max="1"
+                            min="0"
+                            step="0.001"
+                            onChange={this.props.handleTimeChange}
+                        />
+                        <div className="time-left">-{this.props.formattedTimeLeft}</div>
+                    </section>
+                    <section id="volume-control">
+                        <div className="volume-icon ion-volume-medium"></div>
+                        <input
+                            type="range"
+                            className="volume-bar"
+                            value={this.props.currentVolume}
+                            max="1"
+                            min="0"
+                            step="0.01"
+                            onChange={this.props.handleVolumeChange}
+                        />
+                    </section>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default PlayerBar;
