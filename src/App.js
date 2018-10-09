6c4b8cd1b2b28d558d6baf8109acd59312b84031
Drew Michael Disterhfoft <DrewDisterhoft@Drews-iMac.local>
2018-10-09 17:17:46 -0500

    custom styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+
 
 class App extends Component {
   render() {
